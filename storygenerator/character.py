2138390f0bac4921506288a12482014218def69e
Kent Gang <kent.y.gang@gmail.com>
2024-10-19 14:00:31 -0400

    Add scaffold of basic character

---
+++ storygenerator/character.py
@@ -0,0 +1,34 @@
+
+
+_npcs: dict[str, "StoryCharacter"] = {}
+"""Map a npc's name to a character"""
+
+
+class StoryCharacter:
+    @classmethod
+    def gen_character(cls, profile):
+        """
+        profile:
+          LinkedIn profile or basic description
+        """
+        pass
+
+    def __init__(self, description: str, age: int = 25, personality: str = "A normal person."):
+        self.personality = personality
+        self.age = age
+        self.description = description
+
+    def chat(context: str, character: str | None = None):
+        """
+        context:
+          Tell the character what the context is on what's happening and prompt them to respond
+          OR tell the character what they decided to say and have them add their personality
+        
+        character (optional):
+          Who is the character responding to. Should be the unique name for the story
+        """
+        pass
+
+    def interact_w_environment(env_description):
+        pass
+
